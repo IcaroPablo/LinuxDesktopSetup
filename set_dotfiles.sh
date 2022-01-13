85f44bd9406bade5654ea4af016245777d88f39b
icaro pablo <ic4ro.p4blo@gmail.com>
2022-01-13 11:42:03 -0300

    set_dotfiles.sh script now works properly to user files added .gitignore

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -2,10 +2,18 @@
 
 # create backups of the existing dotfiles in system (defined at dotfiles_list below)
 # takes $HOME as first positional argument
+# TODO problems with root directories, must request sudo
 
 HOME=$1
-echo "\$HOME defined as $HOME"
+dotfiles_folder="$(pwd)/dotfiles"
+dotfiles_backup_folder="$(pwd)/dotfiles_backup"
+
+echo "HOME defined as $HOME"
+echo "dotfiles_folder defined as $dotfiles_folder"
+echo "dotfiles_backup_folder defined as $dotfiles_backup_folder"
 echo ""
+
+#variável com o caminho para a pasta de dotfiles
 
 dotfiles_list=(
 $HOME/.xinitrc
@@ -22,24 +30,44 @@ $HOME/.config/picom/picom.conf
 
 for dotfile in ${dotfiles_list[@]}; do
 
-	# get dotfile name from dotfiles_list and create a new backup name
-	echo "backing up $dotfile"
-	bkpname="$(echo $dotfile)-bkp-$(printf '%(%Y-%m-%d_%H-%M-%S)T')"
-
-	# change dotfile name and move it to dotfiles_backup folder
-	echo "storing $bkpname in $(pwd)/dotfiles_backup"
-	mv $dotfile $bkpname
-	cp $bkpname $(pwd)/dotfiles_backup
-
 	#getting dotfile name
 	dotfile_name="$(echo $dotfile | sed 's/\// /g' | awk '{print $NF}')"
 
 	#getting dotfile location
 	dotfile_location="$(echo $dotfile | sed 's%/[^/]*$%/%')"
+	
+	# change dotfile name and move it to dotfiles_backup folder
+	echo "backing up $dotfile"
+	bkpname="$(echo $dotfile_name)-bkp-$(printf '%(%Y-%m-%d_%H-%M-%S)T')"
+	echo "storing $bkpname in $dotfiles_backup_folder"
+
+	# despite this script can deal with links it only works with source files named alike its links
+	if [ -L "$dotfile" ]; then
+		echo "$dotfile is a link"
+    	dotfile_link_source="$(readlink $dotfile)"
+		echo "link source: $dotfile_link_source"
+		
+		if [ "$(echo $dotfile_link_source)" != "$(echo $dotfiles_folder/$dotfile_name)" ]; then
+			echo "backing up original file"
+			mv $dotfile_link_source $dotfiles_backup_folder
+			mv "$dotfiles_backup_folder/$dotfile_name" "$dotfiles_backup_folder/$bkpname"
+		fi
+
+        echo "deleting link"
+	    rm -rf $dotfile
+
+	else
+		#echo $dotfile
+		#echo $dotfiles_backup_folder
+		mv $dotfile $dotfiles_backup_folder
+		mv "$dotfiles_backup_folder/$dotfile_name" "$dotfiles_backup_folder/$bkpname"
+	fi
+
+	#se não
 
     # link saved version of the dotfile to the correct location
-    echo "creating link fot $dotfile_name at $dotfile_location"
-	ln -s $(pwd)/dotfiles/$dotfile_name $dotfile_location
+    echo "creating link for $dotfiles_folder/$dotfile_name at $dotfile_location"
+	ln -s $dotfiles_folder/$dotfile_name $dotfile_location
     echo ""
 
 done
